7ad514c8ae9f0a2094c5ad455d4ff89e1f7941eb
Joaquin Opazo <ja.opazoy@gmail.com>
2023-05-22 15:31:39 +0200

    update

--- contrib/multi_domain_diag.py
+++ contrib/multi_domain_diag.py
@@ -51,8 +51,7 @@ def multi_domain_osse_diag(
     save_dir=None,
     src_dm=None,
 ):
-    # ckpt = torch.load(ckpt_path)["state_dict"]
-    ckpt = torch.load(ckpt_path)
+    ckpt = torch.load(ckpt_path)["state_dict"]
     print('==== CHECK ====')
     print(ckpt)
     lit_mod.load_state_dict(ckpt)

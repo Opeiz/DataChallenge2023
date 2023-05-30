56d98bb911ddfdd49be648c5acdccb8c321f0e84
Joaquin Opazo <ja.opazoy@gmail.com>
2023-05-30 14:02:21 +0200

    update

--- contrib/multi_domain_diag.py
+++ contrib/multi_domain_diag.py
@@ -64,8 +64,8 @@ def multi_domain_osse_diag(
     norm_dm = src_dm or dm
     lit_mod.norm_stats = norm_dm.norm_stats()
 
-    print("=== TEST ===")
-    print(lit_mod.norm_stats)
+    # print("=== TEST ===")
+    # print(lit_mod.norm_stats)
 
     trainer.test(lit_mod, datamodule=dm)
     tdat = lit_mod.test_data

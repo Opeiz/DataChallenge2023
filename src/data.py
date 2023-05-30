6a98ef8dec59e378b82260fc823d2d12cfa0b89e
Joaquin Opazo <ja.opazoy@gmail.com>
2023-05-30 13:50:25 +0200

    update

--- src/data.py
+++ src/data.py
@@ -220,14 +220,20 @@ class BaseDataModule(pl.LightningDataModule):
 
     def norm_stats(self):
         if self._norm_stats is None:
-            print("============ HIHO =======")
             self._norm_stats = self.train_mean_std()
             # print("Norm stats", self._norm_stats)
         return self._norm_stats
 
     def train_mean_std(self):
-        train_data = self.input_da.sel(self.xrds_kw.get('domain_limits', {})).sel(self.domains['train'])
-        return train_data.sel(variable='tgt').pipe(lambda da: (da.mean().values.item(), da.std().values.item()))
+
+        mean_time = np.mean(self.input_da,axis=0)
+        std_time = np.std(self.input_da,axis=0)
+
+        # train_data = self.input_da.sel(self.xrds_kw.get('domain_limits', {})).sel(self.domains['train'])
+        # (mean_batch, std_batch) = train_data.sel(variable='tgt').pipe(lambda da: (da.mean().values.item(), da.std().values.item()))
+        
+        (mean_batch, std_batch) = mean_time,std_time
+        return (mean_batch, std_batch)
 
     def post_fn(self):
         normalize = lambda item: (item - self.norm_stats()[0]) / self.norm_stats()[1]

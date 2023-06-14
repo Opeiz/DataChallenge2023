8ca2c77e7b49234d38e9a231e4394af0e3f80623
Joaquin Opazo <ja.opazoy@gmail.com>
2023-06-14 15:35:04 +0200

    update

--- src/data.py
+++ src/data.py
@@ -221,7 +221,8 @@ class BaseDataModule(pl.LightningDataModule):
     def norm_stats(self):
         if self._norm_stats is None:
             self._norm_stats = self.train_mean_std()
-            # print("Norm stats", self._norm_stats)
+            print("\n===== STD et MEAN ====")
+            print("Norm stats", self._norm_stats)
         return self._norm_stats
 
     def train_mean_std(self):
@@ -229,9 +230,6 @@ class BaseDataModule(pl.LightningDataModule):
         train_data = self.input_da.sel(self.xrds_kw.get('domain_limits', {})).sel(self.domains['train'])
         (mean_batch, std_batch) = train_data.sel(variable='tgt').pipe(lambda da: (da.mean().values.item(), da.std().values.item()))
         
-        print("===== STD et MEAN ====")
-        print(mean_batch,"/",std_batch)
-
         return (mean_batch, std_batch)
 
     def post_fn(self):
